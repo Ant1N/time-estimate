19c7f68bcdbaa02a4dee6c8e35a82724f827173b
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-12 15:33:52 +0200

    Adding components folder and setting up basic structure in form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,1 +1,25 @@
-// Input form coming soon!
+import React from 'react';
+
+// Interface for Form component 
+
+interface FormInterface {
+
+}
+
+// Input form
+
+const Form = (props: FormInterface) => {
+    return (
+        <div className="form-container">
+            <div className="form-input">
+            <p>Input form coming soon!</p>
+            </div>
+
+            <div className="form-button">
+                <button type="button">Save</button>
+            </div>
+        </div>
+    )
+}
+
+export default Form;
