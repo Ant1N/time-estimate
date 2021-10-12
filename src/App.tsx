0c2fa3018117cbb91d6c936991d6e8f08c988215
Gabrielle Kamph <gabriellekamph@Gabrielles-Air.lan>
2021-10-12 20:54:41 +0200

    Created input form and added function to save estimate button

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,80 @@ import React from 'react';
 import './App.css';
 import Form from './components/Form';
 
+<<<<<<< Updated upstream
 function App() {
+=======
+interface IPost {
+  id: number;
+  title: string;
+}
+
+const defaultPosts: IPost[] = [];
+
+const App = () => {
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(
+    defaultPosts
+  );
+
+  const [loading, setLoading]: [
+    boolean,
+    (loading: boolean) => void
+  ] = React.useState<boolean>(true);
+
+  const [error, setError]: [string, (error: string) => void] = React.useState(
+    ''
+  );
+
+  const cancelToken = axios.CancelToken; //create cancel token
+  const [cancelTokenSource, setCancelTokenSource]: [
+    CancelTokenSource,
+    (cancelTokenSource: CancelTokenSource) => void
+  ] = React.useState(cancelToken.source());
+
+  const handleCancelClick = () => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel('User cancelled operation');
+    }
+  };
+
+  React.useEffect(() => {
+    axios
+      .get<IPost[]>('https://api.github.com/repos/Ant1N/time-estimate/issues', {
+        cancelToken: cancelTokenSource.token,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        timeout: 10000,
+      })
+      .then((response) => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        let error = axios.isCancel(ex)
+          ? 'Request Cancelled'
+          : ex.code === 'ECONNABORTED'
+          ? 'A timeout has occurred'
+          : ex.response.status === 404
+          ? 'Resource Not Found'
+          : 'An unexpected error has occurred';
+
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  // Function to handle when new estimated is saved (after button click)
+
+  function handleSaveEstimate() {
+    console.log("Estimate (not yet) saved! (but it's supposed to when this function is done :))");
+  }
+
+>>>>>>> Stashed changes
   return (
     <div className="App">
       <h1>Hello world</h1>
-      <Form />
+      <Form estimate={1} handleSaveEstimate={handleSaveEstimate} />
     </div>
 );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 // Interface for Form component 
 
 interface FormInterface {
-
+    estimate: number;
+    handleSaveEstimate: () => void;
 }
 
 // Input form
@@ -12,11 +13,15 @@ const Form = (props: FormInterface) => {
     return (
         <div className="form-container">
             <div className="form-input">
-            <p>Input form coming soon!</p>
+                <input
+                    name="estimate"
+                    type="text"
+                    placeholder="Your estimate (hours)"
+                />
             </div>
 
             <div className="form-button">
-                <button type="button">Save</button>
+                <button id="save-estimate-btn" type="button" onClick={props.handleSaveEstimate}>Save estimate</button>
             </div>
         </div>
     )
