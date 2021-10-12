d299c111b817b0a2154df236d82c6af9766c9100
Isak Antin <isakantin@Isaks-MacBook-Air.local>
2021-10-12 15:10:21 +0200

    added fetch from github-api, containing id and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,79 @@
 import React from 'react';
-import './App.css';
+import axios, { CancelTokenSource } from 'axios';
 
-function App() {
-  return (
-   <h1>Hello world</h1>
-  );
+interface IPost {
+  id: number;
+  title: string;
 }
+
+const defaultPosts: IPost[] = [];
+
+const App = () => {
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(
+    defaultPosts
+  );
+
+  const [loading, setLoading]: [
+    boolean,
+    (loading: boolean) => void
+  ] = React.useState<boolean>(true);
+
+  const [error, setError]: [string, (error: string) => void] = React.useState(
+    ''
+  );
+
+  const cancelToken = axios.CancelToken; //create cancel token
+  const [cancelTokenSource, setCancelTokenSource]: [
+    CancelTokenSource,
+    (cancelTokenSource: CancelTokenSource) => void
+  ] = React.useState(cancelToken.source());
+
+  const handleCancelClick = () => {
+    if (cancelTokenSource) {
+      cancelTokenSource.cancel('User cancelled operation');
+    }
+  };
+
+  React.useEffect(() => {
+    axios
+      .get<IPost[]>('https://api.github.com/repos/Ant1N/time-estimate/issues', {
+        cancelToken: cancelTokenSource.token,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        timeout: 10000,
+      })
+      .then((response) => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        let error = axios.isCancel(ex)
+          ? 'Request Cancelled'
+          : ex.code === 'ECONNABORTED'
+          ? 'A timeout has occurred'
+          : ex.response.status === 404
+          ? 'Resource Not Found'
+          : 'An unexpected error has occurred';
+
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      {loading && <button onClick={handleCancelClick}>Cancel</button>}
+      <ul className="posts">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+          </li>
+        ))}
+      </ul>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
 
 export default App;
